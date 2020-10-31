bd5b2aa4dc03d7f9ab54f08e9e7699cb92190898
18120158binh <55783367+18120158binh@users.noreply.github.com>
2020-10-31 13:07:25 +0700

    Update Source.c

--- Source.c
+++ Source.c
@@ -10,7 +10,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-char *History = NULL;
+char* History = NULL;
 
 //ham tach cau lenh thanh token theo white space DELIM
 #define TOKEN_BUFSIZE 64
@@ -53,70 +53,69 @@ char** parseSpace(char* line)
     return tokenArr;
 }
 
-void Delete_invalidCharacter(char *s){
-      int s_length = strlen(s) - 1;
-      if(s[s_length] == '\n')
-           s[s_length] = '\0';
+void Delete_invalidCharacter(char* s) {
+    int s_length = strlen(s) - 1;
+    if (s[s_length] == '\n')
+        s[s_length] = '\0';
 }
- 
+
 
 int main() {
-int need_to_wait = 1;
-char cmd[30];
-char **argv = NULL;
-printf("\nWelcome to My Simple Shell ~ ~ ~\n\n");
-while(1){
+    int need_to_wait = 1;
+    char cmd[30];
+    char** argv = NULL;
+    printf("\nWelcome to My Simple Shell ~ ~ ~\n\n");
+    while (1) {
 
-printf("My Shell > ");
-fgets(cmd, sizeof(cmd), stdin);
+        printf("My Shell > ");
+        fflush(stdin);
+        fgets(cmd, sizeof(cmd), stdin);
+        Delete_invalidCharacter(cmd);
+        argv = parseSpace(cmd);
 
-Delete_invalidCharacter(cmd);
-argv = parseSpace(cmd);
-pid_t pid = fork();
+        if (cmd[0] == '\0') continue;
+        if (strcmp(cmd, "exit") == 0) break;
 
-if(cmd[0] == '\0') continue;
-if(strcmp(cmd, "exit") == 0) break; 
+        if (strcmp(cmd, "!!") != 0 && cmd[0] != (char)'\0') {
+            if (History != NULL) free(History);
+            History = (char*)malloc(strlen(cmd) + 1);
+            strcpy(History, cmd);
+        }
 
-if(strcmp(cmd, "!!") != 0 && cmd[0] != (char)'\0'){
-	if (History != NULL) free(History);
-        History = (char*)malloc(strlen(cmd) + 1);
-        strcpy(History, cmd);
-}
-
-if(strcmp(cmd, "!!") == 0){
-	if(History == NULL) {
-		printf("No command in history!\n");
-		continue;
-	}
-	else 
-	{
-		strcpy(cmd, History);
+        if (strcmp(cmd, "!!") == 0) {
+            if (History == NULL) {
+                printf("No command in history!\n");
+                continue;
+            }
+            else
+            {
+                strcpy(cmd, History);
                 printf("My Shell > ");
                 printf(cmd);
                 printf("\n");
-	}
-}
-
-if (pid < 0)
-    {
-        printf("*** ERROR: forking child process failed\n");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        execvp(cmd, argv);
-        printf("*** ERROR: INVALID COMMAND\n");
-        exit(EXIT_FAILURE);
-    }
-    //parent process
-    else {
-	if (need_to_wait)
-    	{
-            while(wait(NULL) != pid);
-	}
-        else {
-            printf("[1]%d\n",pid);
+            }
         }
-    } 
-}
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            printf("*** ERROR: forking child process failed\n");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            execvp(cmd, argv);
+            printf("*** ERROR: INVALID COMMAND\n");
+            exit(EXIT_FAILURE);
+        }
+        //parent process
+        else {
+            if (need_to_wait)
+            {
+                while (wait(NULL) != pid);
+            }
+            else {
+                printf("[1]%d\n", pid);
+            }
+        }
+    }
 }
