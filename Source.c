8685bab5d81e3b54f3f85d49bcf8e96b4ef548e6
18120158binh <55783367+18120158binh@users.noreply.github.com>
2020-10-31 12:25:12 +0700

    Update Source.c

--- Source.c
+++ Source.c
@@ -10,22 +10,47 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-void Separate_Command(char cmd[30], char* argv[5]) {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    while (cmd[i] != '\0') {
-        if (cmd[i] != ' ') {
-            argv[k][j] = cmd[i];
-            i++;
-            j++;
-        }
-        if (cmd[i] == ' ') {
-            argv[k][j] = '\0';
-            j = 0;
-            k++;
-        }
+char *History = NULL;
+
+//ham tach cau lenh thanh token theo white space DELIM
+#define TOKEN_BUFSIZE 64
+#define DELIM " \t\r\n\a"
+char** parseSpace(char* line)
+{
+    int bufSize = TOKEN_BUFSIZE;
+    int pos = 0;
+    char* token;
+    char** tokenArr = (char**)malloc(sizeof(char*) * bufSize);
+
+    if (!tokenArr)
+    {
+        perror("alloction error");
+        exit(EXIT_FAILURE);
     }
+
+    token = strtok(line, DELIM);
+
+    while (token != NULL)
+    {
+        tokenArr[pos] = token;
+        pos++;
+
+        if (pos >= bufSize)
+        {
+            bufSize += TOKEN_BUFSIZE;
+            tokenArr = (char**)realloc(tokenArr, bufSize * sizeof(char*));
+            if (!tokenArr)
+            {
+                perror("allocation error");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, DELIM);
+
+    }
+    tokenArr[pos] = NULL;
+
+    return tokenArr;
 }
 
 void Delete_invalidCharacter(char* s) {
@@ -39,19 +64,44 @@ int main() {
     int need_to_wait = 1;
     char cmd[30];
     char temp[30];
-    char* argv[5] = { NULL };
+    char **argv =  NULL;
     printf("\nWelcome to My Simple Shell ~ ~ ~\n\n");
     while (1) {
-
         printf("My Shell > ");
         fgets(cmd, sizeof(cmd), stdin);
+        
         Delete_invalidCharacter(cmd);
-        argv[0] = cmd;
-        //Separate_Command(cmd, argv);
+        //Chia tach command
+        argv = parseSpace(cmd);
         pid_t pid = fork();
-
+        
+        if(cmd[0] == '\0') continue;
         if (strcmp(cmd, "exit") == 0) { break; }
-
+        
+        //Copy command moi nhat vao History
+        if(strcmp(cmd, "!!") != 0 && cmd[0] != 0)
+        {
+            if(History != NULL) free(History);
+            History = (char*)malloc(strlen(cmd) + 1);
+            strcpy(History, cmd);
+        }
+        
+        //Neu nguoi dung nhap lenh lay History
+        if(strcmp(cmd, "!!") == 0){
+            if(History == NULL)
+            {
+                printf("No command in History!\n");
+                continue;
+            }
+            else{
+                strcpy(cmd, History);
+                printf("My Shell > ");
+                printf(cmd);
+                printf("\n");
+            }
+        }
+        
+        //Chay tien trinh con
         if (pid < 0)
         {
             printf("*** ERROR: forking child process failed\n");
