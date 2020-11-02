83b5cba86695f29b02efd0d6ecc81d24a6a507de
18120158binh <55783367+18120158binh@users.noreply.github.com>
2020-11-02 15:12:31 +0700

    Update Source.c
    Redirect

--- Source.c
+++ Source.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<stdio.h>
 #include<unistd.h>
 #include<string.h>
 #include<stdlib.h>
@@ -12,6 +11,136 @@
 #include <stdbool.h>
 
 char* History = NULL;
+int need_to_wait = 1;
+
+void Exec_NormalCommand(char* Command, char** Argv) {
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        printf("*** ERROR: forking child process failed\n");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        execvp(Command, Argv);
+        printf("*** ERROR: INVALID COMMAND\n");
+        exit(EXIT_FAILURE);
+    }
+    //parent process
+    else {
+        if (need_to_wait)
+        {
+            while (wait(NULL) != pid);
+        }
+        else {
+            printf("[1]%d\n", pid);
+        }
+    }
+}
+
+void Redirect_Input(char** Command, char** FileName) {
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        printf("*** ERROR: forking child process failed\n");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        int Input_fds = open(FileName[0], O_RDONLY);
+        if (Input_fds < 0) {
+            printf("*** ERROR: Cannot open files.\n");
+            exit(EXIT_FAILURE);
+        }
+        if (dup2(Input_fds, STDIN_FILENO) < 0) {
+            printf("*** ERROR: Unable to duplicate file descriptor.\n");
+            exit(EXIT_FAILURE);
+        }
+        execvp(Command[0], Command);
+        printf("*** ERROR: INVALID COMMAND\n");
+        exit(EXIT_FAILURE);
+    }
+    //parent process
+    else {
+        if (need_to_wait)
+        {
+            while (wait(NULL) != pid);
+        }
+        else {
+            printf("[1]%d\n", pid);
+        }
+    }
+}
+
+void Redirect_Output(char** Command, char** FileName) {
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        printf("*** ERROR: forking child process failed\n");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        int Output_fds = open(FileName[0], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+        if (Output_fds < 0) {
+            printf("*** ERROR: Cannot open files.\n");
+            exit(EXIT_FAILURE);
+        }
+        if (dup2(Output_fds, STDOUT_FILENO) < 0) {
+            printf("*** ERROR: Unable to duplicate file descriptor.\n");
+            exit(EXIT_FAILURE);
+        }
+        execvp(Command[0], Command);
+        printf("*** ERROR: INVALID COMMAND\n");
+        exit(EXIT_FAILURE);
+    }
+    //parent process
+    else {
+        if (need_to_wait)
+        {
+            while (wait(NULL) != pid);
+        }
+        else {
+            printf("[1]%d\n", pid);
+        }
+    }
+}
+
+int CheckCommand(char* s) {
+    int s_length = strlen(s) - 1;
+    int i;
+    for (i = 0; i < s_length; i++) {
+        if (s[i] == '>') return 2;
+        if (s[i] == '<') return 1;
+        if (s[i] == '|') return 3;
+    }
+    return 0;
+}
+
+//ham tach cau lenh thanh token theo white space DELIM
+#define TOKEN_BUFSIZE 64
+char** parse(char* Comamnd)
+{
+    int bufSize = TOKEN_BUFSIZE;
+    int pos = 0;
+    char* token;
+    char** tokenArr = (char**)malloc(sizeof(char*) * bufSize);
+
+    if (!tokenArr)
+    {
+        printf("Alloction Error!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(Comamnd, "<>");
+    tokenArr[0] = token;
+
+    token = strtok(NULL, "\0");
+    tokenArr[1] = token;
+    tokenArr[2] = NULL;
+
+    return tokenArr;
+}
 
 //ham tach cau lenh thanh token theo white space DELIM
 #define TOKEN_BUFSIZE 64
@@ -42,7 +171,7 @@ char** parseSpace(char* line)
             tokenArr = (char**)realloc(tokenArr, bufSize * sizeof(char*));
             if (!tokenArr)
             {
-                perror("allocation error");
+                printf("Allocation Error");
                 exit(EXIT_FAILURE);
             }
         }
@@ -56,22 +185,24 @@ char** parseSpace(char* line)
 
 void Delete_invalidCharacter(char* s) {
     int s_length = strlen(s) - 1;
-    if (s[s_length] == '\n')
+    if (s[s_length] == '\n' || s[s_length] == ' ')
         s[s_length] = '\0';
+    if(s[0] == ' ') s = strtok(s, " ");
 }
-
+ 
 
 int main() {
-    int need_to_wait = 1;
     char cmd[30];
     char** argv = NULL;
+    char** argv1 = NULL;
+    char** argv2 = NULL;
     printf("\nWelcome to My Simple Shell ~ ~ ~\n\n");
-    while (1){
+    while (1) {
+
         printf("My Shell > ");
-        fflush(stdin);
         fgets(cmd, sizeof(cmd), stdin);
+
         Delete_invalidCharacter(cmd);
-        argv = parseSpace(cmd);
 
         if (cmd[0] == '\0') continue;
         if (strcmp(cmd, "exit") == 0) break;
@@ -95,27 +226,27 @@ int main() {
                 printf("\n");
             }
         }
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            printf("*** ERROR: forking child process failed\n");
-            exit(EXIT_FAILURE);
+
+        int TypeCmd = CheckCommand(cmd);
+        if (TypeCmd == 1) {
+            argv = parse(cmd);
+            Delete_invalidCharacter(argv[0]);
+            argv1 = parseSpace(argv[0]);
+            Delete_invalidCharacter(argv[1]);
+            argv2 = parseSpace(argv[1]);
+            Redirect_Input(argv1, argv2);
         }
-        else if (pid == 0)
-        {
-            execvp(cmd, argv);
-            printf("*** ERROR: INVALID COMMAND\n");
-            exit(EXIT_FAILURE);
+        if (TypeCmd == 2) {
+            argv = parse(cmd);
+            Delete_invalidCharacter(argv[0]);
+            argv1 = parseSpace(argv[0]);
+            Delete_invalidCharacter(argv[1]);
+            argv2 = parseSpace(argv[1]);
+            Redirect_Output(argv1, argv2);
         }
-        //parent process
-        else {
-            if (need_to_wait)
-            {
-                while (wait(NULL) != pid);
-            }
-            else {
-                printf("[1]%d\n", pid);
-            }
+        if (TypeCmd == 0) {
+            argv1 = parseSpace(cmd);
+            Exec_NormalCommand(cmd, argv1);
         }
     }
 }
